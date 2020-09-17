a3a053c82e87aa36276b200c187817119557fb6f
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-09-17 11:40:59 +0200

    Add QT Thread

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,7 @@
-/** @file paex_record.c
-	@ingroup examples_src
-	@brief Record input into an array; Save array to a file; Playback recorded data.
-	@author Phil Burk  http://www.softsynth.com
-*/
-
-/*
- * $Id$
- *
- * This program uses the PortAudio Portable Audio Library.
- * For more information see: http://www.portaudio.com
- * Copyright (c) 1999-2000 Ross Bencina and Phil Burk
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files
- * (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
- * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
- * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
-/*
- * The text above constitutes the entire PortAudio license; however, 
- * the PortAudio community also makes the following non-binding requests:
- *
- * Any person wishing to distribute modifications to the Software is
- * requested to send the modifications to the original developer so that
- * they can be incorporated into the canonical version. It is also 
- * requested that these non-binding requests be included along with the 
- * license above.
- */
-
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QPushButton>
 #include "portaudio.h"
 
 /* #define SAMPLE_RATE  (17932) // Test failure to open with this value. */
@@ -193,11 +151,23 @@ static int playCallback( const void *inputBuffer, void *outputBuffer,
 }
 
 /*******************************************************************/
-int main(void);
-int main(void)
+
+void QT_app(int argc, char **argv)
 {
-    PaStreamParameters  inputParameters,
-                        outputParameters;
+    QApplication app(argc, argv);
+    QPushButton button("Hello world pouet pouet !");
+    button.show();
+    app.exec();
+}
+
+#include <thread>
+
+int main(int argc, char **argv)
+{
+    std::thread first(QT_app, argc, argv);
+
+    PaStreamParameters inputParameters;
+    PaStreamParameters outputParameters;
     PaStream*           stream;
     PaError             err = paNoError;
     paTestData          data;
@@ -350,6 +320,7 @@ done:
         fprintf( stderr, "Error message: %s\n", Pa_GetErrorText( err ) );
         err = 1;          /* Always return 0 or 1, but no other return codes. */
     }
+    first.join();
     return err;
 }
 
