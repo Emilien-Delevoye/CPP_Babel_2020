c115e80a613d1fe104220035f06f404c8f8881cf
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-09-25 19:51:19 +0200

    Remove useless comments and update constructor call for PortAudio encapsulation

--- src/PortAudio.cpp
+++ src/PortAudio.cpp
@@ -8,7 +8,8 @@
 #include <iostream>
 #include "PortAudio.hpp"
 
-PortAudio::PortAudio() : captured(this->FRAME_SIZE * this->CHANNEL_NB), decoded(this->FRAME_SIZE * this->CHANNEL_NB)
+PortAudio::PortAudio() : captured(this->FRAME_SIZE * this->CHANNEL_NB),
+                         decoded(this->FRAME_SIZE * this->CHANNEL_NB)
 {
     this->init();
 }
@@ -20,41 +21,6 @@ void PortAudio::init()
     if (err != paNoError)
         throw std::exception();
 }
-
-/*
-void PortAudio::tmp()
-{
-    std::vector<unsigned char> encoded(this->FRAME_SIZE * this->CHANNEL_NB * 2);
-    int opusError = OPUS_OK;
-    opus_int32 enc_bytes;
-    PaError err;
-
-    OpusEncoder *enc = opus_encoder_create(this->SAMPLE_RATE, this->CHANNEL_NB, OPUS_APPLICATION_AUDIO, &opusError);
-    if (opusError != OPUS_OK) {
-        std::cerr << opusError << std::endl;
-        throw std::exception("Opus encode creation");
-    }
-
-    OpusDecoder *dec = opus_decoder_create(this->SAMPLE_RATE, this->CHANNEL_NB, &opusError);
-    if (opusError != OPUS_OK) {
-        std::cerr << opusError << std::endl;
-        throw std::exception("Opus decode creation");
-    }
-
-    for (int i = 0; i < (this->NUM_SECONDS * this->SAMPLE_RATE) / this->FRAMES_PER_BUFFER; ++i) {
-
-        if ((enc_bytes = opus_encode(enc, reinterpret_cast<opus_int16 const *>(captured.data()),
-            this->FRAME_SIZE, encoded.data(), encoded.size())) < 0)
-            throw std::exception("opus_encode");
-        if (opus_decode(dec, encoded.data(), enc_bytes, reinterpret_cast<opus_int16 *>(decoded.data()), this->FRAME_SIZE, 0) < 0)
-            throw std::exception("opus_decode");
-    }
-    std::cout << "Wire off." << std::endl;
-    err = Pa_StopStream(stream);
-    if (err)
-        throw std::exception(Pa_GetErrorText(err));
-}
-*/
 
 void PortAudio::startStream()
 {
