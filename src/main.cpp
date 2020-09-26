17c8869b4ccec1bc601ed1adc626177fecd77b0b
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-09-26 19:47:16 +0200

    Add demonstration code for Opus and PortAudio class

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,29 @@
 */
 
 #include "PortAudio.hpp"
+#include "Opus.hpp"
 #include <iostream>
 
 int main(int argc, char **argv)
 {
-    std::cout << "Hello World" << std::endl;
+    Opus opus;
+    PortAudio portAudio;
+
+    opus.createEncoder();
+    opus.createDecoder();
+    portAudio.startStream();
+    for (int i = 0; i < ((3000 * 48000) / 512); ++i) {
+        portAudio.readStream();
+        opus.setCaptured(portAudio.getCaptured());
+        opus.encodeData();
+        opus.decodeData();
+        portAudio.setDecoded(opus.getDecoded());
+        portAudio.writeStream();
+        std::cout << "loop" << std::endl;
+    }
+    portAudio.stopStream();
+    std::cout << "End stream" << std::endl;
+    portAudio.~PortAudio();
+    opus.~Opus();
     return 0;
 }
