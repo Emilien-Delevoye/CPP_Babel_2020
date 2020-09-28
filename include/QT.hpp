1920ec88552c189bf859e898bc6793a47fa653ce
Paul Surrans <paul@localhost.localdomain>
2020-09-28 12:28:47 +0200

    Add QT branch

---
+++ include/QT.hpp
@@ -0,0 +1,13 @@
+//
+// Created by paul on 9/28/20.
+//
+
+#ifndef BABEL_QT_HPP
+#define BABEL_QT_HPP
+
+class QT {
+    public:
+        QT();
+};
+
+#endif //BABEL_QT_HPP
---
+++ src/QT.cpp
@@ -0,0 +1,9 @@
+//
+// Created by paul on 9/28/20.
+//
+
+#include "QT.hpp"
+
+QT::QT() {
+
+}
