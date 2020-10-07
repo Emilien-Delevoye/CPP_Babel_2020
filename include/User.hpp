1921cea31fae586c6c0a3755ae6b76c2edc89ffb
Paul Surrans <paul.surrans@epitech.eu>
2020-10-07 09:39:51 +0200

    Add <br> to commentary in include/User.hpp

--- include/User.hpp
+++ include/User.hpp
@@ -4,10 +4,11 @@
  * @author Paul.S
  * @version 1.0
  * @date 05/10/2020
- * This class is useful to describe how the User is represented in our program.
+ * This class is useful to describe how the User is represented in our program.<br>
  * This class is composed of two methods (getter) and two member variables (login and ip).
  *
 */
+
 
 #ifndef BABEL_USER_HPP
 #define BABEL_USER_HPP
