2bbffcb30cd0ac11b8f783a0b681c685bad8e5a8
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-10-09 12:01:31 +0200

    Add comments for doxygen in Encode.cpp

--- client/src/Audio/Encode.cpp
+++ client/src/Audio/Encode.cpp
@@ -12,6 +12,10 @@
 #include <iostream>
 #include <utility>
 
+/*!
+* \brief Encode::Encode constructor sets the default values
+*/
+
 Encode::Encode() : encoded(this->FRAME_SIZE * this->CHANNEL_NB * 2),
                captured(this->FRAME_SIZE * this->CHANNEL_NB),
                decoded(this->FRAME_SIZE * this->CHANNEL_NB)
@@ -20,6 +24,10 @@ Encode::Encode() : encoded(this->FRAME_SIZE * this->CHANNEL_NB * 2),
     this->dec = nullptr;
     this->encBytes = 0;
 }
+
+/*!
+* \brief Encode::createEncoder init the encoder
+*/
 
 void Encode::createEncoder()
 {
@@ -33,6 +41,10 @@ void Encode::createEncoder()
     //TMP -> Surveiller ce paramètre, il est intéressant sur le taux de compression
 }
 
+/*!
+* \brief Encode::createDecoder init the decoder
+*/
+
 void Encode::createDecoder()
 {
     int opusError = OPUS_OK;
@@ -44,6 +56,10 @@ void Encode::createDecoder()
     }
 }
 
+/*!
+* \brief Encode::encodeData encode the data set by setCaptured setter
+*/
+
 void Encode::encodeData()
 {
     this->encBytes = opus_encode(enc, reinterpret_cast<opus_int16 const *>(this->captured.data()), this->FRAME_SIZE, encoded.data(), static_cast<opus_int32>(encoded.size()));
@@ -52,16 +68,30 @@ void Encode::encodeData()
         throw EncodeError("Encode: ", "Error : Encode encode error.");
 }
 
+/*!
+* \brief Encode::decodeData decode the data set by setToDecode setter
+*/
+
 void Encode::decodeData()
 {
     if (opus_decode(dec, this->toDecode.data(), static_cast<opus_int32>(this->toDecBytes), reinterpret_cast<opus_int16 *>(this->decoded.data()), this->FRAME_SIZE, 0) < 0)
         throw EncodeError("Encode: ", "Error : Encode decode error.");
 }
 
+/*!
+* \brief Encode::getEncoded return the encoded vector
+*/
+
 std::vector<unsigned char> Encode::getEncoded()
 {
     return this->encoded;
 }
+
+/*!
+* \brief Encode::setToDecode set the encoded vector to decode
+* \param intToDecode is the encoded vector
+* \param encBytesFromUDP is the number of bytes to decode 
+*/
 
 void Encode::setToDecode(std::vector<unsigned char> inToDecode, size_t encBytesFromUDP)
 {
@@ -69,15 +99,28 @@ void Encode::setToDecode(std::vector<unsigned char> inToDecode, size_t encBytesF
     this->toDecBytes = encBytesFromUDP;
 }
 
+/*!
+* \brief Encode::setCaptured set the vector to encode
+* \param inCaptured is the vector to encode
+*/
+
 void Encode::setCaptured(std::vector<unsigned short> inCaptured)
 {
     this->captured = std::move(inCaptured);
 }
 
+/*!
+* \brief Encode::getDecoded returns the vector decoded
+*/
+
 std::vector<unsigned short> Encode::getDecoded()
 {
     return this->decoded;
 }
+
+/*!
+* \brief Encode::getEncBytes returns the number of bytes encoded
+*/
 
 size_t Encode::getEncBytes() const
 {
