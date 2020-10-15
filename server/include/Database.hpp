cc2c443d961a94616114e638bed90f15823f3dea
Cyprien Ricque <cyprien.ricque@epitech.eu>
2020-10-15 16:03:43 +0200

    Document Database

--- server/include/Database.hpp
+++ server/include/Database.hpp
@@ -47,22 +47,59 @@ class DataBase : ADatabase<T> {
 public:
     DataBase();
 
+    /*!
+     * \brief addRow method
+     * \param user
+     *
+     * Add user in db.
+    */
     template<typename V>
     V addRow(const T *user) {
         return storage.insert(*user);
     }
+
+    /*!
+     * \brief addRow method
+     * \param id
+     *
+     * remove row with id "id".
+    */
     void removeRow(const int id) {
         storage.remove<T>(id);
     }
+
+    /*!
+     * \brief getVarFromId method
+     * \param varType
+     * \param id
+     * \param idType
+     * \param errorValue
+     *
+     * get var for a specific of specific id
+    */
     template<typename P, typename U, typename N, typename J>
     P getVarFromId(const U varType, const N id, const J idType, const P errorValue) {
         auto v = storage.select(varType, where(c(idType) == id));
         return v.begin() == v.end() ? errorValue : *(v.begin());
     }
+
+    /*!
+     * \brief getDataFromId method
+     * \param id
+     * \param idType
+     *
+     * get row from id
+    */
     template<typename U, typename V>
     T getDataFromId(const V id, const U idType) {
         return storage.get_all<T>(where(c(idType) == id));
     }
+    /*!
+     * \brief getColumn method
+     * \param varType
+     *
+     * get whole column "type"
+    */
     template<typename B, typename V>
     std::vector<B> getColumn(const V varType) {
         return storage.select(varType);
